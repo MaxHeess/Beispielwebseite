d8bcc2d16a9c159b1e6a09a33257c66777e1e61d
Maximilian Heeß <max-heess@gmx.de>
2025-05-15 08:53:17 +0200

    Update angaben.php

--- angaben.php
+++ angaben.php
@@ -2,10 +2,10 @@
 session_start();  // Sitzung starten
 
 // Datenbankverbindung herstellen
-$servername = "db3299.mydbserver.com";
-$username = "p683530";
-$password = "PaSSw0rd.2025";
-$dbname = "usr_p683530_1";
+$servername = 
+$username =
+$password = 
+$dbname =
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password);
